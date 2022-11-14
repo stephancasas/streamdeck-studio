6b713825dcc4a17ac8763ec9ae804df8dd070e39
Stephan Casas <stephancasas@icloud.com>
2022-11-14 01:48:23 -0600

    add campaign to acknowledgements

--- resources/views/components/acknowledgements.blade.php
+++ resources/views/components/acknowledgements.blade.php
@@ -1,7 +1,27 @@
-<div class="border-t border-theme flex items-center h-8 text-xs text-center px-2">
-    <div class="flex-grow">Created by <a href="https://github.com/stephancasas" target="_blank">Stephan
-            Casas</a></div>
+<div class="border-t border-theme flex items-center h-8 text-[8pt] text-center px-2">
+    <div class="flex-grow w-0 truncate">
+        <span>Powered by</span>
+        <a href="https://www.fontawesome.com/" target="_blank">
+            FontAwesome Pro
+        </a>
+    </div>
+
     <div class="border-l border-theme-light w-0 h-full"></div>
-    <div class="flex-grow">Powered by <a href="https://www.fontawesome.com/" target="_blank">FontAwesome
-            Pro</a></div>
+
+    <div class="flex-grow w-0 truncate">
+        <span>
+            Created by
+        </span>
+        <a href="https://github.com/stephancasas" target="_blank">
+            Stephan Casas
+        </a>
+    </div>
+
+    <div class="border-l border-theme-light w-0 h-full"></div>
+
+    <div class="flex-grow w-0 truncate">
+        <a href="https://buymeacoffee.com/stephancasas" target="_blank">
+            Buy Stephan a Coffee
+        </a>
+    </div>
 </div>
