ed5667caa8721376c34bae1b132331d20dfe365c
Stephan Casas <stephancasas@icloud.com>
2022-11-14 02:00:41 -0600

    add github repo link

--- resources/views/components/theme-control.blade.php
+++ resources/views/components/theme-control.blade.php
@@ -1,4 +1,4 @@
-<div class="flex items-center relative h-full w-[50px] ml-auto border-l border-theme overflow-hidden"
+<div class="flex items-center relative h-full w-[50px] border-l border-theme overflow-hidden"
      x-data="Theme"
      @click="toggle()">
     <div class="app-theme-set inset-button-theme"
--- resources/views/studio.blade.php
+++ resources/views/studio.blade.php
@@ -1,13 +1,26 @@
 <x-app-layout>
     <header class="flex items-center border-b border-theme h-[50px]">
         <x-identity />
-        
+
         {{-- font preload workaround --}}
         <div class="h-0 opacity-0 overflow-hidden">
             <span style="font-family: 'Geo';">{{ config('app.name') }}</span>
             <span style="font-family: 'VT323';">{{ config('app.name') }}</span>
         </div>
 
+        {{-- GitHub Button --}}
+        <a @class([
+            'relative h-full w-[50px] ml-auto',
+            'flex items-center',
+            'border-l border-theme',
+            'inset-button-theme',
+        ]) href="https://github.com/stephancasas/streamdeck-studio" target="_blank">
+            <div @class(['flex items-center', 'w-5 mx-auto'])>
+                @fontawesome('github', 'brands')
+            </div>
+        </a>
+
+        
         <x-theme-control />
     </header>
 
